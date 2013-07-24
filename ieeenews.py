e90a25ca4ca0d26cce0f36537530c7b7890cc02a
Shaurya Shahi <shahi.shaurya@gmail.com>
2013-07-24 12:35:53 +0530

    Updated with sqlite3

--- ieeenews.py
+++ ieeenews.py
@@ -1,20 +1,24 @@
 from lxml.html import parse
 import sys
 import codecs
+import sqlite3
 
 def main():
+	headings = []
+	htt = []
+	conn = sqlite3.connect('./database/news.db')
+	c = conn.cursor()
 	sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 	url = 'http://www.ieee.org/about/news/index.html'
 	page = parse(url).getroot()
 	thediv = page.cssselect('div.box-lc-indent')[1][1]
 	#Prints the Recent Headlines from IEEE
 	for r in thediv.cssselect('p>strong'):
-		heading = r.text_content().split()
+		heading = r.text_content().strip()
 		if not heading:
 			continue
 		else:
-			print ' '.join(heading)
-			print 
+			headings.append(heading)
 	#Prints the HyperLinks to Full Story
 	for r in thediv.cssselect('p'):
 		links = r.cssselect('a')
@@ -22,10 +26,15 @@ def main():
 			if link.text_content()[0] == 'R':
 				link = link.attrib['href']
 				if link[0] == '/':
-					htt = 'http://www.ieee.org%s'
-					htt = htt %(link)
+					http = 'http://www.ieee.org%s'
+					htt.append(http %(link))
 				else:
-					htt = link
-				print htt
+					htt.append(link)
+	for i in xrange(14):
+		print headings[i], htt[i]
+		a = headings[i]
+		b = htt[i]
+		c.execute('insert into news (headings,hlink) values (?,?)',(a,b))
+		conn.commit()
 if __name__ == '__main__':
 	main()
